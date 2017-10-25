759b43ff537a6b0770989fc05dc62aa38d08bde3
feifeiiiiiiiiiii <qinpengfei.2008@gmail.com>
2017-10-25 22:22:59 +0800

    Complete Distributing MapReduce jobs

--- 6.824/src/mapreduce/master.go
+++ 6.824/src/mapreduce/master.go
@@ -28,7 +28,72 @@ func (mr *MapReduce) KillWorkers() *list.List {
 	return l
 }
 
+func (mr *MapReduce)doTask(worker string, index int, jobType JobType, numPhase int) bool {
+	var job DoJobArgs
+	var reply DoJobReply
+
+	job.File = mr.file
+	job.Operation = jobType
+	job.JobNumber = index
+	job.NumOtherPhase = numPhase
+
+	return call(worker, "Worker.DoJob", job, &reply)
+}
+
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+
+	// 启动map worker
+	var mapChan = make(chan int, mr.nMap)
+	for i := 0; i < mr.nMap; i++ {
+		go func(idx int) {
+			for {
+				var worker string
+				var ok bool = false
+
+				select {
+				case worker = <- mr.registerChannel:
+					ok = mr.doTask(worker, idx, Map, mr.nReduce)
+				case worker = <- mr.idleChannel:
+					ok = mr.doTask(worker, idx, Map, mr.nReduce)
+				}
+				if ok {
+					mapChan <- idx
+					mr.idleChannel <- worker
+					return
+				}
+			}
+		}(i)
+	}
+	for i := 0; i < mr.nMap; i++{
+		<- mapChan
+	}
+
+	// 启动reduce worker
+	var reduceChan = make(chan int, mr.nReduce)
+	for i := 0; i < mr.nReduce; i++ {
+		go func(idx int) {
+			for {
+				var worker string
+				var ok bool = false
+
+				select {
+				case worker = <- mr.registerChannel:
+					ok = mr.doTask(worker, idx, Reduce, mr.nMap)
+				case worker = <- mr.idleChannel:
+					ok = mr.doTask(worker, idx, Reduce, mr.nMap)
+				}
+				if ok {
+					reduceChan <- idx
+					mr.idleChannel <- worker
+					return
+				}
+			}
+		}(i)
+	}
+	for i := 0; i < mr.nReduce; i++{
+		<- reduceChan
+	}
+		
 	return mr.KillWorkers()
 }
